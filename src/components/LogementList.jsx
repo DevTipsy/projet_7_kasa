62b2e9f07b8f038a3b3103a7c77bc8c729f13566
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2022-11-18 15:27:01 +0100

    update 6
    Fix grid css & header

--- src/components/LogementList.jsx
+++ src/components/LogementList.jsx
@@ -7,7 +7,7 @@ function LogementList() {
   return (
     <div className="Logementlist">
       {data.map(({ title, cover, id }) => (
-        <div key={`location_${id}`}>
+        <div className='logementLink' key={`location_${id}`}>
           <Link to={`/Logement/${id}`}>
             <LogementCard cover={cover} title={title} id={id} />
           </Link>
